0b10580cd731ff5c70f983955cf812b9d8422b50
Neil Lindquist <NeilLindquist5@gmail.com>
2019-05-14 15:17:58 -0500

    Add basic evaluation test

--- spec/ClientSpec.js
+++ spec/ClientSpec.js
@@ -1,7 +1,7 @@
 const {spawn} = require('child_process');
 const Swank = require('../lib/client');
 
-describe('connecting', function(){
+describe('Swank Client', function(){
 
   swank = new Swank.Client("localhost", 4005);
 
@@ -40,17 +40,39 @@ describe('connecting', function(){
     lisp_proc.kill();
   });
 
-  it('Can connect', function() {
+  it('can connect', function() {
     expect(swank.connected).toBe(true);
   });
 
-  it('Can disconnect', function() {
-    disconnect_handler = jasmine.createSpy('disconnect_handler').and.returnValue(undefined);
+  it('can disconnect', function(done) {
+    disconnect_handler = function() {
+      expect(swank.connected).toBe(false);
+      done();
+    }
     swank.on('disconnect', disconnect_handler);
 
-    swank.quit().then(function() {
-      expect(swank.disconnect).toBe(false);
-      expect(disconnect_handler.calls.count()).toEqual(1);
+    swank.quit()
+  });
+
+  it('can eval', function(done) {
+    print_handler = jasmine.createSpy('print_handler');
+    swank.on('print_string', print_handler);
+    pres_start_handler = jasmine.createSpy('pres_start_handler');
+    swank.on('presentation_start', pres_start_handler);
+    pres_end_handler = jasmine.createSpy('pres_end_handler');
+    swank.on('presentation_end', pres_end_handler);
+
+    swank.eval('(list (+ 1 1) "4")\n').then(function (){
+      expect(pres_start_handler.calls.count()).toEqual(1);
+      expect(pres_start_handler.calls.argsFor(0)).toMatch(/\d+/);
+      expect(pres_end_handler.calls.count()).toEqual(1);
+      expect(pres_end_handler.calls.argsFor(0)).toEqual(pres_start_handler.calls.argsFor(0));
+
+      expect(print_handler.calls.count()).toEqual(2);
+      expect(print_handler.calls.argsFor(0)).toEqual(['(2 "4")']);
+      expect(print_handler.calls.argsFor(1)).toEqual(['\n']);
+
+      done();
     });
   });
 });
